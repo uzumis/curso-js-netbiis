cd66cccc4c378654f002e4f11eed52024cd3de6d
Thiago Calado <thiago.calado@netbiis.com>
2024-11-11 21:23:14 -0300

    [MODULE-ERROR] - updated ErrorHandler.js

--- Thiago Calado Sosinho/CURSO 4/UNIDADE 4/api_candidatos/src/middlewares/ErrorHandler.js
+++ Thiago Calado Sosinho/CURSO 4/UNIDADE 4/api_candidatos/src/middlewares/ErrorHandler.js
@@ -1,11 +1,12 @@
-import AppError from "../errors/AppError.js"
+import AppError from "../errors/AppError.js";
+
 const ErrorHandler = (err, req, res, next) => {
-    if (err instanceof AppError) {
-        res.status9(err.satusCode).send({ message: err.message });
+    if(err instanceof AppError) {
+        res.status(err.statusCode).send({message: err.message});
     } else {
-        console.log(err);
-        res.status(500).send({ message: "Internal server error" });
+        console.error(err);
+        res.status(500).send({message: 'Internal server error'});
     }
-}
+};
 
 export default ErrorHandler;
